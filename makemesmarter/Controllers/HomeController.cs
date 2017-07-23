498b4581666a26eb8cd46d19d0864de5ee41327c
ashishonce <ashishonce@gmail.com>
2017-07-23 18:17:02 +0530

    adding email sending logic without database

--- makemesmarter/Controllers/HomeController.cs
+++ makemesmarter/Controllers/HomeController.cs
@@ -9,6 +9,10 @@ using System.Web.Mvc;
 using makemesmarter.Models;
 using System.Threading.Tasks;
 using makemesmarter.Helpers;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Collections.Generic;
 
 namespace makemesmarter.Controllers
 {
@@ -130,9 +134,44 @@ namespace makemesmarter.Controllers
             return resultString.ToString();
         }
 
-        public async Task SendMail()
+        public async Task<string> SendMail()
         {
+            var listmail = new List<EmailAddress>();
+            var mailAddressList = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("ashkuma@microsoft.com", "ashish kumar"),
+                new Tuple<string, string>("nazikh@microsoft.com", "nazia khan"),
+                new Tuple<string, string>("umkan@microsoft.com", "umesh kanoja"),
+                new Tuple<string, string>("riniga@microsoft.com", "richa nigam")
+            };
+
+            foreach(var mailAddress in mailAddressList)
+            {
+                EmailAddress testemail = new EmailAddress();
+                testemail.Email = mailAddress.Item1;
+                testemail.Name = mailAddress.Item2;
+                listmail.Add(testemail);
+            }
+            
+            var content = MailContentCreator.CreatePendingCommentResponse(); ;
+      
+            await SendEmail(listmail, "Pending CR comments", content);
             this.Response.StatusCode = (int)HttpStatusCode.OK;
+            return content;
+        }
+
+        private static async Task SendEmail(List<EmailAddress> recipients, string subject, string mailContent)
+        {
+            var client = new SendGridClient("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");
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("noreply@azure.com", "CodeFlow Stalker"),
+                Subject = subject,
+                HtmlContent = mailContent
+            };
+
+            msg.AddTos(recipients);
+            var response = await client.SendEmailAsync(msg);
         }
 
         protected override void Dispose(bool disposing)
---
+++ makemesmarter/Helpers/MailContentCreator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace makemesmarter.Helpers
+{
+
+    public static class MailContentCreator
+    {
+        public static string CreatePendingCommentResponse()
+        {
+            var content = "<html>";
+            content += Getstyle();
+            content += "<h2> Pending CR summary</h2>";
+            content += "<table>";
+            content += addHeader();
+            content += addRow("12345", "This is funny comment", "newfile");
+            content += "</table>";
+            content += addDisclaimer();
+            content += "</html>";
+
+            return content;
+        }
+
+        private static string Getstyle()
+        {
+            var style = "<style> th,td{ padding:10px; border: 1px solid; }";
+            style += "table{ border: 1px solid black; } </style> ";
+
+            return style;
+        }
+
+        private static string addDisclaimer()
+        {
+            return "<p> Discalimer : The detials are subjective to changes; currently we don not support link on the mail</p>";
+        }
+
+        private static string addHeader()
+        {
+            return "<tr> <th> PR_ID</th> <th>Comment</th> <th> File</th></tr>";
+        }
+
+        private static string addRow(string PRId , string comment, string file )
+        {
+            return string.Format("<tr> <td> {0} </td> <td> {1} </td> <td> {2} </td> </tr>", PRId, comment, file);
+        }
+    }
+}
